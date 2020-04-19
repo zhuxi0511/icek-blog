905b70b1d7f7416f04afee600fff6b65b14a3138
Zhu Xi <zhuxi910511@163.com>
2020-04-19 09:17:02 +0800

    update blog for title and icon

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@*1-$bajrjjukz&r!ykq1he!(gxbse-fx7fg48pkg+2kka297m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
         "www.dudurun.com",
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
         ]
 
 STATIC_ROOT = '/app/run/static_files/'
+
+TRACK_PAGEVIEWS = True
--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,1 +1,2 @@
+docker rm -f blog1.1
 docker run -d --restart=unless-stopped --name blog1.1 -v /home/zhuxi/blog/:/app/blog -p:8080:8080 docker_blog:1.1
